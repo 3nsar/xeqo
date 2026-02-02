488b9442202c1073e536ff30ae8eef13972d07d8
Ensar <ensar_mehmedovic@hotmail.com>
2026-02-02 15:30:14 +0100

    commit

--- xeqo/src/Hero.tsx
+++ xeqo/src/Hero.tsx
@@ -5,6 +5,7 @@ const Hero = () => {
     <div className=''>
       <h1>Xeqo</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
         <p>Adipisci mollitia, sed pariatur placeat odit deleniti hic amet ut molestiae, consequuntur, 
         maxime sunt fuga voluptatem illum esse aspernatur vitae debitis modi.</p>
     </div>
