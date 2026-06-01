9a78f8540c376c76d42cdc4f6833e2f7ffba0d9f
Ensar <ensar_mehmedovic@hotmail.com>
2026-06-01 21:57:14 +0200

    commit

--- xeqo/src/Hero.tsx
+++ xeqo/src/Hero.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className=''>
+      <h1>Xeqo</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+        <p>Adipisci mollitia, sed pariatur placeat odit deleniti hic amet ut molestiae, consequuntur, 
+        maxime sunt fuga voluptatem illum esse aspernatur vitae debitis modi.</p>
+    </div>
   )
 }
 
